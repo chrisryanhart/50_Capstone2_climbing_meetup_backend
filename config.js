3603b41fc9d3edf8c36365424a30d4694fa44f57
rrghiker <chris.ryan.hart1@gmail.com>
2023-01-17 14:36:47 -0500

    updated port in server and config files

--- config.js
+++ config.js
@@ -1,14 +1,21 @@
+"use strict";
+
+require("dotenv").config();
+require("colors"); 
+
+const SECRET_KEY = process.env.SECRET_KEY || "secret";
+
+const PORT = +process.env.PORT || 3001;
 
 const DB_URI = (process.env.NODE_ENV === "test")
 ? "postgresql:///climbing_meetup_test"
 : "postgresql:///climbing_meetup";
-
-const SECRET_KEY = process.env.SECRET_KEY || "secret";
 
 const BCRYPT_WORK_FACTOR = 12;
 
 module.exports = {
 DB_URI,
 SECRET_KEY,
-BCRYPT_WORK_FACTOR
+BCRYPT_WORK_FACTOR,
+PORT
 };
--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const app = require('./app');
+const { PORT } = require('./config');
 
-app.listen(3001, function () {
+app.listen(PORT, function () {
     console.log('App on port 3001');
   });
+
+  // import port from config.js
+
+// "use strict";
+
+// const app = require("./app");
+// const { PORT } = require("./config");
+
+// app.listen(PORT, function () {
+//   console.log(`Started on http://localhost:${PORT}`);
+// });
